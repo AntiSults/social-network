765398f62dbcc44c1dd4d81f273b081806871229
agedz <aleksei.gedz@gmail.com>
2024-09-21 21:08:46 +0300

    backend cleanup

--- backend/handlers/getAvatar.go
+++ backend/handlers/getAvatar.go
@@ -8,34 +8,35 @@ import (
 )
 
 func GetAvatarPath(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		logged := false
-		cookie, err := r.Cookie("session_token")
-		if err != nil {
-			if err != http.ErrNoCookie {
-				middleware.SendErrorResponse(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		} else {
-			logged = true
-		}
-		if logged {
-			userID, err := GetUserId(cookie.Value)
-			if err != nil {
-				middleware.SendErrorResponse(w, "error getting id from token: "+err.Error(), http.StatusInternalServerError)
-				return
-			}
-			avatarPath, err := sqlite.Db.GetAvatarFromID(userID)
-			if err != nil {
-				avatarPath = ""
-			}
-			response := map[string]string{
-				"avatarPath": avatarPath,
-			}
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(response)
+
+	if r.Method != http.MethodGet {
+		middleware.SendErrorResponse(w, "Method not allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	logged := false
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		if err != http.ErrNoCookie {
+			middleware.SendErrorResponse(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		logged = true
+	}
+	if logged {
+		userID, err := GetUserId(cookie.Value)
+		if err != nil {
+			middleware.SendErrorResponse(w, "error getting id from token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		avatarPath, err := sqlite.Db.GetAvatarFromID(userID)
+		if err != nil {
+			avatarPath = ""
+		}
+		response := map[string]string{
+			"avatarPath": avatarPath,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
 	}
 }
